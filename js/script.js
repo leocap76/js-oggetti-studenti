bbb6a15ad95189c8ed36e22ec87ee603151d76b5
Leonardo Capogna <leonardocapogna@gmail.com>
2020-11-27 15:45:57 +0100

    fix

--- js/script.js
+++ js/script.js
@@ -8,3 +8,39 @@
 // Dare la possibilità all’utente attraverso
 // 3 prompt di aggiungere un nuovo oggetto
 // studente inserendo nell’ordine: nome, cognome e età.
+
+var studenti = [
+  {
+    "nome" : "Leonardo",
+    "cognome" : "Capogna",
+    "età" : 25,
+  },
+  {
+    "nome" : "Silvia",
+    "cognome" : "Masci",
+    "età" : 23,
+  },
+  {
+    "nome" : "Alice",
+    "cognome" : "De Angelis",
+    "età" : 26,
+  },
+  {
+    "nome" : "Lorenzo",
+    "cognome" : "Corsetti",
+    "età" : 21,
+  },
+];
+
+//stampa in console tutti gli studenti della lista
+for (var i = 0; i < studenti.length; i++) {
+  console.log(studenti[i].nome, studenti[i].cognome, studenti[i].età);
+}
+
+//inserisci un nuovo studente
+for (var i = 0; i < 1; i++) {
+  studenti.nome = prompt("scrivi il tuo nome");
+  studenti.cognome = prompt("scrivi il tuo cognome");
+  studenti.età = prompt("inserisci la tua età");
+}
+console.log(studenti);
